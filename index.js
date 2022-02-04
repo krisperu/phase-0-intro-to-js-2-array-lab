1f3502a662c895b8f32d249a5b29a3c177180356
Kristina Peru <krissy.germanov@gmail.com>
2022-02-03 22:25:11 -0700

    commit tree da1f7f5a723e8b324a21dc7e902531879c66f299

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat() {
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat() {
+    let moreCats = [...cats, 'Broom'];
+    return moreCats;
+}
+
+function prependCat() {
+    let moreCats = ['Arnold', ...cats];
+    return moreCats;
+}
+
+function removeLastCat() {
+    let moreCats = cats.slice(0, -1);
+    return moreCats;
+}
+
+function removeFirstCat() {
+    let moreCats = cats.slice(1);
+    return moreCats;
+}
